2384262273581d986f9cf696aee96815b8c4c22a
Tomas <otasssss@gmail.com>
2023-10-05 08:42:02 +0300

    Unrestricted admin endpoint

---
+++ VulnerableAPI/VulnerableAPI/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VulnerableAPI.Database;
+
+namespace VulnerableAPI.Controllers;
+
+[ApiController]
+[Route("v2/admin")]
+public class AdminController : ControllerBase
+{
+    private readonly IConfiguration _config;
+    private readonly AdminDbContext _context;
+    public AdminController(AdminDbContext context, IConfiguration config)
+    {
+        _config = config;
+        _context = context;
+    }
+
+    [HttpGet("users")]
+    public async Task<ActionResult> Login()
+    {
+        Response.Headers.Add("BrokenFunctionLevelAuthorization", _config["Flags:BrokenFunctionLevelAuthorization"]);
+        var users = await _context.Users.Select(u => new { u.Email, u.FirstName, u.LastName }).ToListAsync();
+        return Ok(users);
+    }
+}
